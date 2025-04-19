115a759351077648b6d391c48583da6e03b7f049
Ken Kambi <kenkambi43@gmail.com>
2025-04-19 19:51:50 +0300

    Updated Navbar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,12 @@
-import Navbar from "./Navbar"
 import Fanfacts from "./Facts"
+import Navbar from "./Navbar"
 import "./style.css"
 
 function App() {
 
   return (
     <div className="App">
-      <Navbar /> 
+      <Navbar />
       <Fanfacts />
      
     </div>
--- react/src/Navbar.jsx
+++ react/src/Navbar.jsx
@@ -1,9 +1,8 @@
-import {react} from "./assets/react.svg";
 
 export default function  Navbar() {
   return (
     <nav>
-     <img src={react} alt="React Logo" />
+     <img src="./src/assets/react.svg" alt="React Logo" />
     </nav>
   );
 }
