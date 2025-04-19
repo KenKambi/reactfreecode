3690030ecfff1e9a8d6bd4871f070d3dfe27abdb
Ken Kambi <kenkambi43@gmail.com>
2025-04-19 21:43:23 +0300

    finishing touches

--- react/src/Facts.jsx
+++ react/src/Facts.jsx
@@ -6,13 +6,13 @@ export default function Funfacts() {
         <div className="facts">
             <h1> Fun facts about React</h1>
             <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originaly created by Jordan Walke</li>
-                <li>Has well over 100k stars on Github </li>
-                <li>Is maintained by <Meta:edge></Meta:edge></li>
-                <li>Powers thousands of enterprise apps, including mobile apps</li>
+                <li>Was first released in 2013.</li>
+                <li>Was originaly created by Jordan Walke.</li>
+                <li>Has well over 100k stars on Github. </li>
+                <li>Is maintained by Meta.</li>
+                <li>Powers thousands of enterprise apps, including mobile apps.</li>
             </ul>
-            <small> &copy; {new Date().getFullYear()}. This is my website</small>
+            <small> &copy; {new Date().getFullYear()}. All rights reserved.</small>
         </div>
     );
 }
