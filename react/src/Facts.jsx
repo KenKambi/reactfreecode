1e5353559d619579098b1e3d62bf0ffc2b236e62
KenKambi <122309458+KenKambi@users.noreply.github.com>
2025-05-02 23:14:31 +0300

    Update Facts.jsx

--- react/src/Facts.jsx
+++ react/src/Facts.jsx
@@ -7,7 +7,7 @@ export default function Funfacts() {
             <h1> Fun facts about React</h1>
             <ul>
                 <li>Was first released in 2013.</li>
-                <li>Was originaly created by Jordan Walke.</li>
+                <li>Was originaly created by Jordan Walke*.</li>
                 <li>Has well over 100k stars on Github. </li>
                 <li>Is maintained by Meta.</li>
                 <li>Powers thousands of enterprise apps, including mobile apps.</li>
